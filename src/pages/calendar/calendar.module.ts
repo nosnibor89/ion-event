86bb67342e201a8f0d64883ef1b980cef62142f1
Robinson Marquez <robinson.marquez@moduscreate.com>
2018-06-20 18:13:57 -0400

    Support for iOS platfom; fix imports issues

--- src/pages/calendar/calendar.module.ts
+++ src/pages/calendar/calendar.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CalendarPage } from './calendar';
-import { MainNavComponent } from '../../shared/components/main-nav/main-nav';
-import { ProfilePage } from '../profile/profile';
 
 @NgModule({
   declarations: [
     CalendarPage,
-    MainNavComponent
   ],
   imports: [
     IonicPageModule.forChild(CalendarPage),
